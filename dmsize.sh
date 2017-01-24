3cb015767dd66fbf5023b776c7abc2544abeeb05
zhouwei <zhouwei8@le.com>
2017-01-24 18:58:48 +0800

    add

--- dmsize.sh
+++ dmsize.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # show Direct Memory Size
 # see: DirectMemorySize.java
+# created by zhouwei on 2017-1-24
 
 if [[ ! -f ./target/direct-memory-size-0.0.1-SNAPSHOT.jar ]]; then
     mvn clean package
